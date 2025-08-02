d93ea371e5a52e3b73286e3e580f8362c2fa829e
neyron8 <neyron832@gmail.ocm>
2025-08-03 00:11:36 +0700

    SignUP Screen design was changed

--- app/src/main/java/com/example/movieapplication/navigation/SignUpScreen.kt
+++ app/src/main/java/com/example/movieapplication/navigation/SignUpScreen.kt
@@ -1,16 +1,16 @@
 package com.example.movieapplication.navigation
 
 import android.util.Patterns
-import android.widget.Toast
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
@@ -19,6 +19,8 @@ import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -29,17 +31,15 @@ import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -48,8 +48,7 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.example.movieapplication.MainViewModel
-import com.google.firebase.auth.auth
-import com.google.firebase.Firebase
+
 
 @Composable
 fun SignUpScreen(mainViewModel: MainViewModel = hiltViewModel(), navController: NavController) {
@@ -60,126 +59,151 @@ fun SignUpScreen(mainViewModel: MainViewModel = hiltViewModel(), navController: 
     val isFormValid = email.isNotBlank() && password.isNotBlank()
     val isEmailValid = Patterns.EMAIL_ADDRESS.matcher(email).matches()
 
-    Column(
+
+    Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(24.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Spacer(Modifier.height(56.dp))
-
-        //Spacer(Modifier.height(16.dp))
-
-        /*Text(
-            text = "Firebase Authentication",
-            style = MaterialTheme.typography.titleMedium,
-            color = Color.DarkGray
-        )*/
-
-        Text(
-            text = "Регистрация",
-            style = MaterialTheme.typography.headlineSmall.copy(
-                fontWeight = FontWeight.Bold,
-            )
-        )
-
-        Spacer(Modifier.height(24.dp))
-
-        TextField(
-            shape = MaterialTheme.shapes.medium,
-            value = email,
-            onValueChange = { email = it },
-            label = { Text("Email") },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
-            leadingIcon = {
-                Icon(
-                    Icons.Filled.Email,
-                    contentDescription = null,
-                    tint = Color(0xFF6200EE)
-                )
-            },
-            colors = TextFieldDefaults.colors(
-                focusedTextColor = Color.Black,
-                unfocusedTextColor = Color.Black,
-                focusedContainerColor = Color.LightGray,
-                unfocusedContainerColor = Color.LightGray,
-                cursorColor = Color(0xFF6200EE),
-                errorCursorColor = Color.Red,
-                focusedIndicatorColor = Color.Transparent,
-                unfocusedIndicatorColor = Color.Transparent,
-                focusedLabelColor = Color.Black,
-                unfocusedLabelColor = Color.Black,
-            ),
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        Spacer(Modifier.height(16.dp))
-
-        TextField(
-            value = password,
-            onValueChange = { password = it },
-            label = { Text("Пароль") },
-            visualTransformation = if (passwordHidden) PasswordVisualTransformation() else VisualTransformation.None,
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-            leadingIcon = {
-                Icon(Icons.Filled.Lock, contentDescription = null, tint = Color(0xFF6200EE))
-            },
-            trailingIcon = {
-                IconButton(onClick = { passwordHidden = !passwordHidden }) {
-                    Icon(
-                        imageVector = if (passwordHidden) Icons.Filled.Visibility else Icons.Filled.VisibilityOff,
-                        contentDescription = if (passwordHidden) "Show" else "Hide",
-                        tint = Color(0xFF6200EE)
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        Color(0xFF2A2A2A),
+                        Color(0xFFBB86FC)
                     )
-                }
-            },
-            colors = TextFieldDefaults.colors(
-                focusedTextColor = Color.Black,
-                unfocusedTextColor = Color.Black,
-                focusedContainerColor = Color.LightGray,
-                unfocusedContainerColor = Color.LightGray,
-                cursorColor = Color(0xFF6200EE),
-                errorCursorColor = Color.Red,
-                focusedIndicatorColor = Color.Transparent,
-                unfocusedIndicatorColor = Color.Transparent,
-                focusedLabelColor = Color.Black,
-                unfocusedLabelColor = Color.Black,
-            ),
-            modifier = Modifier.fillMaxWidth(),
-            shape = MaterialTheme.shapes.medium,
-        )
-
-        Spacer(Modifier.height(24.dp))
-
-        Button(
-            onClick = {
-                mainViewModel.signUp(
-                    email = email,
-                    password = password,
-                    navController = navController,
-                    context = context
                 )
-            },
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color(0xFF6200EE),
-                contentColor = Color(0xFFFFFFFF),
-            ),
-            enabled = isFormValid && isEmailValid,
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text("Регистрация")
-        }
-
-        TextButton(onClick = {
-            navController.popBackStack()
-            email = ""
-            password = ""
-        }
-        ) {
-            Text(
-                text = "Уже есть аккаунт? Вход",
-                color = Color(0xFF6200EE)
             )
-        }
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 24.dp, vertical = 48.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(16.dp))
+                    .shadow(8.dp, RoundedCornerShape(16.dp))
+                    .background(Color(0xFF2A2A2A))
+                    .padding(24.dp),
+                colors = CardDefaults.cardColors(containerColor = Color(0xFF2A2A2A))
+            ){
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ){
+                    Spacer(Modifier.height(8.dp))
+                    Text(
+                        text = "Регистрация",
+                        style = MaterialTheme.typography.headlineSmall.copy(
+                            fontWeight = FontWeight.Bold,
+                            color = Color(0xFFBB86FC)
+                        )
+                    )
+
+                    Spacer(Modifier.height(24.dp))
+
+                    TextField(
+                        shape = MaterialTheme.shapes.medium,
+                        value = email,
+                        onValueChange = { email = it },
+                        label = { Text("Email") },
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                        leadingIcon = {
+                            Icon(
+                                Icons.Filled.Email,
+                                contentDescription = null,
+                                tint = Color(0xFFBB86FC)
+                            )
+                        },
+                        colors = TextFieldDefaults.colors(
+                            focusedTextColor = Color.White,
+                            unfocusedTextColor = Color.White,
+                            focusedContainerColor = Color(0xFF4A4A4A),
+                            unfocusedContainerColor = Color(0xFF4A4A4A),
+                            cursorColor = Color(0xFFBB86FC),
+                            errorCursorColor = Color.Red,
+                            focusedIndicatorColor = Color.Transparent,
+                            unfocusedIndicatorColor = Color.Transparent,
+                            focusedLabelColor = Color(0xFFBB86FC),
+                            unfocusedLabelColor = Color(0xFFBB86FC),
+                        ),
+                        modifier = Modifier.fillMaxWidth()
+                    )
+
+                    Spacer(Modifier.height(16.dp))
+
+                    TextField(
+                        value = password,
+                        onValueChange = { password = it },
+                        label = { Text("Пароль") },
+                        visualTransformation = if (passwordHidden) PasswordVisualTransformation() else VisualTransformation.None,
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                        leadingIcon = {
+                            Icon(Icons.Filled.Lock, contentDescription = null, tint = Color(0xFFBB86FC))
+                        },
+                        trailingIcon = {
+                            IconButton(onClick = { passwordHidden = !passwordHidden }) {
+                                Icon(
+                                    imageVector = if (passwordHidden) Icons.Filled.Visibility else Icons.Filled.VisibilityOff,
+                                    contentDescription = if (passwordHidden) "Show" else "Hide",
+                                    tint = Color(0xFFBB86FC)
+                                )
+                            }
+                        },
+                        colors = TextFieldDefaults.colors(
+                            focusedTextColor = Color.White,
+                            unfocusedTextColor = Color.White,
+                            focusedContainerColor = Color(0xFF4A4A4A),
+                            unfocusedContainerColor = Color(0xFF4A4A4A),
+                            cursorColor = Color(0xFFBB86FC),
+                            errorCursorColor = Color.Red,
+                            focusedIndicatorColor = Color.Transparent,
+                            unfocusedIndicatorColor = Color.Transparent,
+                            focusedLabelColor = Color(0xFFBB86FC),
+                            unfocusedLabelColor = Color(0xFFBB86FC),
+                        ),
+                        modifier = Modifier.fillMaxWidth(),
+                        shape = MaterialTheme.shapes.medium,
+                    )
+
+                    Spacer(Modifier.height(24.dp))
+
+                    Button(
+                        onClick = {
+                            mainViewModel.signUp(
+                                email = email,
+                                password = password,
+                                navController = navController,
+                                context = context
+                            )
+                        },
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color(0xFFBB86FC),
+                            contentColor = Color.Black,
+                            disabledContainerColor = Color(0xFFB0BEC5),
+                            disabledContentColor = Color.White
+                        ),
+                        enabled = isFormValid && isEmailValid,
+                        modifier = Modifier.fillMaxWidth()
+                            .height(48.dp),
+                        shape = RoundedCornerShape(12.dp)
+
+                    ) {
+                        Text("Регистрация")
+                    }
+
+                    TextButton(onClick = {
+                        navController.popBackStack()
+                        email = ""
+                        password = ""
+                    }
+                    ) {
+                        Text(
+                            text = "Уже есть аккаунт? Вход",
+                            color = Color(0xFFBB86FC)
+                        )
+                    }
+                }
+            }
     }
-}
+}}
